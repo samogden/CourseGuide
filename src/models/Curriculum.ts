ca756378abe4205b3ec95f8b198ffa2dce7df08d
Sam <samuel.s.ogden@gmail.com>
2026-07-23 11:39:53 -0700

    Getting rid of old file

--- src/models/Curriculum.ts
+++ src/models/Curriculum.ts
@@ -82,3 +82,42 @@ const degree_plan: DegreePlan = new DegreePlan(
 )
 
 export { overall_plan }
+
+/*
+ Loading up data from the yaml files
+*/
+
+// import { parse } from "yaml"
+// import coursesText from "../assets/courses.yaml?raw"
+
+// interface CourseData {
+//   name: string;
+//   units: number;
+//   description?: string;
+// }
+
+// const catalog = (parse(coursesText) as {
+//   courses: {
+//     catalog: Record<string, CourseData>;
+//   };
+// }).courses.catalog;
+
+// const allCourses: Record<string, Course> = Object.fromEntries(
+//   Object.entries(catalog).map(
+//     ([code, data]) => [code, Course.fromObject(code, data)]
+//   )
+// );
+
+// for (const course of Object.values(allCourses)) {
+//   for (const prereq_name of Object.values(course.raw_prereqs)) {
+//     console.log("preque_name: " + prereq_name)
+//     if (Object.keys(allCourses).includes(prereq_name)) {
+//       // Then the pre-req already exists and we can just pull it out
+//       course.prerequisites.push(allCourses[prereq_name])
+//     } else {
+//       // Then we'll have to make a dummy pre-req
+//       course.prerequisites.push(new DummyCourse(prereq_name, 3))
+//     }
+//   }
+// }
+/* end loading */
